b029f698ae235be84968c2b1de606050299d15d6
Shireesha <bongaralashireesha@gmail.com>
2015-06-06 13:22:11 +0530

    Shireesha/Refactored code

--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -3,9 +3,6 @@ package com.twu.biblioteca;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static org.junit.Assert.assertThat;
 
 public class BooksTest {
@@ -14,7 +11,7 @@ public class BooksTest {
     public void testIfBookDetailsArePrintedOnConsole() {
         Books books = new Books();
 
-    String  actualListOfBooks ="C Balagurusamy    Balagurusamy    1990\nC++    Balagurusamy    1995\nJava    O'Reilly    2012\n" ;
+        String  actualListOfBooks ="C Balagurusamy    Balagurusamy    1990\nC++    Balagurusamy    1995\nJava    O'Reilly    2012\n" ;
 
         assertThat(actualListOfBooks.toString(), CoreMatchers.is(books.toString()));
     }
