439fe728d03215f07e78ad0f7fe8f6b4675eb555
Shireesha <bongaralashireesha@gmail.com>
2015-06-07 13:26:43 +0530

    Shireesha/Added spec for displaying list of books in 'BibliotecaOutputHandlerTest.java'

--- src/com/twu/biblioteca/BibliotecaOutputHandler.java
+++ src/com/twu/biblioteca/BibliotecaOutputHandler.java
@@ -1,10 +1,19 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaOutputHandler {
 
+    public ArrayList<Book> listOfBooks = new ArrayList<Book>();
 
     public void displayWelcomeMessage() {
         System.out.print("Welcome to Biblioteca Library Management System");
+    }
+    public void displayListOfBooks() {
+        listOfBooks.add(new Book("C Balagurusamy","Balagurusamy",1990));
+        listOfBooks.add(new Book("C++","Balagurusamy",1995));
+        listOfBooks.add(new Book("Java","O'Reilly",2012));
+        System.out.print(new Books(listOfBooks));
     }
 
 }
