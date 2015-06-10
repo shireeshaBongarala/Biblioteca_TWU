788cc58dd2b675dc4d2caa4e1328f7c0edddc860
Shireesha <bongaralashireesha@gmail.com>
2015-06-10 22:33:22 +0530

    Added spec for displaying error message when choice entered is neither one or two

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -40,4 +40,18 @@ public class BibliotecaAppTest {
 
         Mockito.verify(bibliotecaOutputHandlerStub,atLeast(1)).display(Messages.QUIT_MESSAGE);
     }
+
+    @Test
+    public void shouldDisplayErrorMessageWhenChoiceIsNeitherOneOrTwo(){
+        Books expectedBooks = new Books(BibliotecaApp.getListOfBooks());
+        MainMenu mainMenuStub = mock(MainMenu.class);
+        BibliotecaOutputHandler bibliotecaOutputHandlerStub = mock(BibliotecaOutputHandler.class);
+        when(mainMenuStub.getChoice(bibliotecaOutputHandlerStub))
+                .thenReturn(3,2);
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(
+                bibliotecaOutputHandlerStub, mainMenuStub, expectedBooks);
+
+        Mockito.verify(bibliotecaOutputHandlerStub,atLeast(1)).display(Messages.ERROR_MESSAGE);
+    }
 }
