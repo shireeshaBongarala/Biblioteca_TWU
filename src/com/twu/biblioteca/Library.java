4caa4b2dfa51229b6c647f6ccb4daca9c13a851f
Shireesha <bongaralashireesha@gmail.com>
2015-06-16 11:12:14 +0530

    Added spec in library should add book to available book list when the book is valid checked out book

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,4 +1,29 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> availableBookList;
+    private ArrayList <Book> checkedOutBookList;
+    Library(){
+        availableBookList = new ArrayList<Book>();
+        checkedOutBookList = new ArrayList<Book>();
+        checkedOutBookList.add(new Book("Let Us C","balagurusamy",1997));
+        availableBookList.add(new Book("C Balagurusamy", "Balagurusamy", 1990));
+        availableBookList.add(new Book("C++", "Balagurusamy", 1995));
+        availableBookList.add(new Book("Java", "O'Reilly", 2012));
+    }
+    public ArrayList<Book> getAvailableBookList(){
+        return availableBookList;
+    }
+
+    public boolean addToAvailableBookList(String bookName) {
+        for(Book book : checkedOutBookList)
+        if (book.getName().equals(bookName)) {
+            availableBookList.add(book);
+            checkedOutBookList.remove(book);
+            return true;
+        }
+        return false;
+    }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,4 +1,31 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
 public class LibraryTest {
+    @Test
+    public void shouldBeInitializedWithStaticBookList() {
+        Library library = new Library();
+        ArrayList <Book> books;
+
+        books = library.getAvailableBookList();
+
+        assertFalse(books.isEmpty());
+    }
+    @Test
+    public void shouldAddToBookListWhenItIsValidCheckedOutBook() {
+        Library library = new Library();
+
+        String bookName = "Let Us C";
+
+        assertThat(library.addToAvailableBookList(bookName),is(true));
+    }
+
 }
+
+
